f449efe23a1ec41c78044892690880c8b5c3c620
Matthias Dellago <126871781+matthiasdellago@users.noreply.github.com>
2023-04-29 17:16:38 +0200

    Create strong_attractor.glsl

---
+++ strong_attractor.glsl
@@ -0,0 +1,48 @@
+// Domonstration a strong attractor.
+// One attention vector  is stationay at (6,0).
+// Another is orbiting the origin with a radius of 3.
+// When close to the larger attention vector the smaller vectors field is eclipsed,
+// and it no longer forms it's own attractor.
+// Play with the parameters and see what happens!
+// Check out more visualisations at github.com/matthiasdellago/visualising-attention
+
+vec2 attention(vec2 p) {
+  
+  // init v
+  vec2 v = vec2(0., 0.);
+  
+  // scale defined separately to easily change it. Try it!
+  float scale = 3.;
+  
+  // using frame counter to create oscillating movement.
+  float freq = 0.0005;
+  float amplitude = 2.;
+  float osc = amplitude*fract(frame*freq);
+  
+  //define attention vectors
+  vec2 f1 = scale * vec2(2.,0.);
+  vec2 f2 = scale * vec2(cos(2.*3.14*osc),sin(2.*3.14*osc));
+
+  // calc projections -> weights
+  float w1 = dot(p,f1);
+  float w2 = dot(p,f2);
+
+  // softmax
+  float sum = exp(w1)+exp(w2);
+  w1 = exp(w1)/sum;
+  w2 = exp(w2)/sum;
+
+  // linear combination
+  vec2 new_p = w1*f1 + w2*f2;
+  
+  // calc delta of p
+  v = new_p - p;
+
+  return v;
+}
+
+vec2 get_velocity(vec2 p){
+  // get p.x and p.y, the current coordinates
+  // return v.x and v.y, the velocity at point p
+  return attention(p);
+}
